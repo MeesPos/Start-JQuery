e88829edbf79072516b17046a1556310445cead3
Mees Postma <35431488+MeesPos@users.noreply.github.com>
2020-03-03 20:17:42 +0100

    Eigen JQuery en van video toegevoegd

--- js/mijnHelden.js
+++ js/mijnHelden.js
@@ -1,3 +1,5 @@
+// In de les gemaakt
+
 $('h1').click ( function() {
     $('main').hide(900).delay(600).show(1200);
 });
@@ -5,3 +7,41 @@ $('h1').click ( function() {
 $('#tim p').click(function(){
     $('#tim img').slideToggle(700);
 })
+
+// Buiten de les gemaakt
+
+// Via de video van Theo
+
+// Mouse Events
+
+$('figure img').mouseenter( function() {
+    $(this).fadeTo(600, .01);
+}).mouseleave(function() {
+    $(this).fadeTo(1000, 1);
+}).click( function() {
+    $(this).next().toggle(700);
+});
+
+// Footer
+
+$('footer').click( function() {
+    $(this).animate( {
+    top: '-=50px',
+    opacity: '-=.2'
+    }, 900)
+} ).dblclick ( function() {
+    $(this).animate( {
+    top: '0',
+    opacity: '1'
+    }, 900)
+});
+
+// Eigen code
+
+$("h3").dblclick(function(){
+    $(this).hide();
+})
+
+$("button").click(function(){
+    alert("Value: " + $("#test").val());
+});
